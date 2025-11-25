f9e7de48e401f607847710b78d6aa190e885dd81
riku <r22180917@gmail.com>
2025-11-25 23:56:12 +0900

    adjust

--- review/main_12_review.go
+++ review/main_12_review.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	// "os"
 	// "time"
 	// "strconv"
 	// "math/rand"
-	"sync/atomic"
 	"github.com/labstack/echo/v4"
+	"sync/atomic"
 )
 
 type Todo struct {
-	ID int `json: "id"`
-	Title string `json: "title"`
-	Completed bool `json: "completed"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
+
 // Todo一覧用の箱
 var todos []Todo
-var nextID int64=0
+var nextID int64 = 0
 
 func main() {
 	e := echo.New()
@@ -30,7 +32,11 @@ func main() {
 		}
 		// 自動採番
 		newID := atomic.AddInt64(&nextID, 1)
-		todo.ID = int(newID) 
+		todo.ID = int(newID)
+		// review comment
+		// ただ todos = append(todos, todo) は並列POSTが来ると本当は危ない（race）です。
+		// 今はこのままで全然OK。
+		// 本番だとここに Mutex か DB が要るんだな」くらいの理解で十分です。
 		todos = append(todos, todo)
 
 		return c.JSON(http.StatusOK, todo)
