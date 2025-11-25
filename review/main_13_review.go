197dc2a8caddc5531887837a93e6bd0ccd4f7933
riku <r22180917@gmail.com>
2025-11-26 00:40:37 +0900

    adjust 13

---
+++ review/main_13_review.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"net/http"
+	// "os"
+	// "time"
+	"strconv"
+	// "math/rand"
+	"github.com/labstack/echo/v4"
+	"sync/atomic"
+)
+
+type Todo struct {
+	ID        int    `json: "id"`
+	Title     string `json: "title"`
+	Completed bool   `json: "completed"`
+}
+
+// Todo一覧用の箱
+var todos []Todo
+var nextID int64 = 0
+
+func main() {
+	e := echo.New()
+	e.POST("/todos", func(c echo.Context) error {
+		var todo Todo
+		if err := c.Bind(&todo); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"error": "invalid json",
+			})
+		}
+		// 自動採番
+		newID := atomic.AddInt64(&nextID, 1)
+		todo.ID = int(newID)
+		todos = append(todos, todo)
+
+		return c.JSON(http.StatusOK, todo)
+	})
+	e.GET("/todos", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, todos)
+	})
+	e.GET("/todos/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		numID, err := strconv.Atoi(id)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"error": "id must be an integer",
+			})
+		}
+
+		// todosの中から探す
+		for _, t := range todos {
+			if t.ID == numID {
+				return c.JSON(http.StatusOK, t)
+			}
+		}
+
+		// 見つからない場合
+		return c.JSON(http.StatusNotFound, map[string]string{
+			"error": "todo not found",
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":3080"))
+}
